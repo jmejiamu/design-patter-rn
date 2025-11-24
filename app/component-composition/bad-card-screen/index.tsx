26f5e3a2b9d32c48c6449bc14933da4bd573379b
jmejiamu <jmejiamu@gmail.com>
2025-11-23 20:30:19 -0800

    first commit

---
+++ app/component-composition/bad-card-screen/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const BadCardScreen = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  );
+};
+
+export default BadCardScreen;
+
+const styles = StyleSheet.create({});
