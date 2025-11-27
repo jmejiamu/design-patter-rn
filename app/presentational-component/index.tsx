23d2c3a5dfcefbb0618d60b6f04a065527e1cd0a
jmejiamu <jmejiamu@gmail.com>
2025-11-26 16:02:13 -0800

    feat/ implement presentational component

--- app/presentational-component/index.tsx
+++ app/presentational-component/index.tsx
@@ -1,12 +1,24 @@
-// UserCardContainer.tsx (Container)
-import React from "react";
+import React, { useEffect } from "react";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { UserCard } from "../../components/UserCard";
 const UserCardContainer = () => {
+  const [user, setuser] = React.useState<any>();
+  useEffect(() => {
+    setTimeout(() => {
+      setuser({
+        name: "John Doe",
+        email: "john@gmail.com",
+        avatar: "https://i.pravatar.cc/300",
+      });
+    }, 1000);
+  }, []);
+  if (!user) return <Text>Loading...</Text>;
   return (
     <SafeAreaView>
       <View style={{ padding: 16 }}>
         <Text>Container Component</Text>
+        <UserCard name={user?.name} email={user?.email} avatar={user?.avatar} />
       </View>
     </SafeAreaView>
   );
